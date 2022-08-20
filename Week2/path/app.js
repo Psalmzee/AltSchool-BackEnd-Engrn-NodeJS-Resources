cdb78d9ba1b65c82ccaf682cb1023e71a42cb25d
Samson Okeji <engrsamsonokeji@gmail.com>
2022-08-20 23:51:57 +0100

    First Commit for Baackend Engrn

---
+++ Week2/path/app.js
@@ -0,0 +1,54 @@
+// const path = require('path');
+const path = require('path');
+
+
+
+// NOTES:
+// Windows uses backslashes (\) as the path separator.
+// Mac and Linux use forward slashes (/) as the path separator.
+// Examples:
+// Windows: ==>  C:\Users\John\Documents\example.txt
+// Mac and Linux: ==>  /Users/John/Documents/example.txt
+
+
+// Get path seperator
+// const seperator = path.sep;
+// const seperator = path.sep;
+// console.log(seperator);
+// console.log(seperator);
+
+// // Get the base name
+// const baseName = path.basename('/Users/risingodegua/Documents/Altschool/Backend_Engineering/path/package.json');
+// const baseName = path.basename('C:\Users\Samson Okeji\Desktop\Alt-School-Africa-Resources\Backend_Engineering\Week2\path\package.json');
+// console.log(baseName);
+// console.log(baseName);
+
+// // Get the directory name
+// const dirName = path.dirname('/Users/risingodegua/Documents/Altschool/Backend_Engineering/path/package.json');
+// console.log(dirName);
+// const dirName = path.dirname('C:\Users\Samson Okeji\Desktop\Alt-School-Africa-Resources\Backend_Engineering\Week2\path\package.json');
+// console.log(dirName);
+
+// // Get the extension name
+// const extName = path.extname('/Users/risingodegua/Documents/Altschool/Backend_Engineering/path/package.json');
+// const extName = path.extname('C:\Users\Samson Okeji\Desktop\Alt-School-Africa-Resources\Backend_Engineering\Week2\path\package.json');
+// console.log(extName);
+// console.log(extName);
+
+// // Join paths
+// const joinPath = path.join('/', 'Backend_Engineering', 'path', 'package.json');
+// console.log(joinPath);
+// const joinPath = path.join('Backend', 'frontend', 'package.json');
+// console.log(joinPath);
+
+// // Get current working directory/folder
+// const cwd = path.resolve();
+// console.log(cwd);
+// const cwd = path.resolve();
+// console.log(cwd);
+
+// // Get the current directory name. The directory hosting the current node.js script.
+// const dir = path.dirname(__dirname);
+// const dir = path.dirname(__dirname);
+// console.log(dir);
+// console.log(dir);
